b8a0cec6080eb9968fae74fcc35026e8a8bf8776
Kamal Pant <kamalpant100@gmail.com>
2026-03-12 20:17:16 +0530

    testing ci/cd for docker flow

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-	  res.send("Hello ji from Kamal DevOps Lab 🚀");
+	  res.send("Hello ji from Kamal DevOps Lab 🚀(Docker)");
 });
 
 app.get("/health", (req, res) => {
