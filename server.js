10d15e8282615dc39b3a03599f7daffd7fac82b6
Kamal Pant <kamalpant100@gmail.com>
2026-03-12 14:43:25 +0530

    initial hello-api service

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+	  res.send("Hello from Kamal DevOps Lab 🚀");
+});
+
+app.get("/health", (req, res) => {
+	  res.json({
+		      status: "OK",
+		      service: "hello-api",
+		    });
+});
+
+app.listen(3000, () => {
+	  console.log("Server running on port 3000");
+});
